696ab5b024426f1bf00395eede6bc244b728b6f0
MaryamMehzabinZahin <50514699+MaryamMehzabinZahin@users.noreply.github.com>
2022-02-15 09:35:17 +0600

    why special method is needed

--- Design_pattern_python/srp.py
+++ Design_pattern_python/srp.py
@@ -1,11 +1,11 @@
-#SRP SOC
+# SRP SOC
 class Journal:
     def __init__(self):
-        self.entries=[]
-        self.count=0
+        self.entries = []
+        self.count = 0
 
-    def add_entry(self,text):
-        self.count+=1
+    def add_entry(self, text):
+        self.count += 1
         self.entries.append(f'{self.count}: {text}')
 
     def remove_entry(self, pos):
@@ -14,7 +14,8 @@ class Journal:
     def __str__(self):
         return '\n'.join(self.entries)
 
-j=Journal()
+
+j = Journal()
 j.add_entry('I cried.')
 j.add_entry('I have read a book.')
 print(f'journal entries:\n{j}')
