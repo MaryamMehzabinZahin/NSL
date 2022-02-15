457ef4f99fe0008d7a3437a85e70d8b5cb6e3f70
MaryamMehzabinZahin <50514699+MaryamMehzabinZahin@users.noreply.github.com>
2022-02-15 14:19:48 +0600

    Update srp.py

--- Design_pattern_python/srp.py
+++ Design_pattern_python/srp.py
@@ -12,10 +12,26 @@ class Journal:
         del self.entries[pos]
 
     def __str__(self):
-        return '\n'.join(self.entries)
+        # return str(self.entries)
+        return 'tt'.join(self.entries)
+
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
 
 
 j = Journal()
 j.add_entry('I cried.')
 j.add_entry('I have read a book.')
 print(f'journal entries:\n{j}')
+
+file = r'C:\Users\NSL\Desktop\nsl\notes5.txt'
+#PersistenceManager.save_to_file(j, file)
+#
+# with open(file) as fh:
+#     print(fh.read())
+
